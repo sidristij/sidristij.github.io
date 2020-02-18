e0a388a611832f114481a655dec68669b407abe7
Stanislav Sidristij <sunex.development@gmail.com>
2020-02-18 11:07:21 +0300

    video loader fixed + async added to mentor

--- js/index.js
+++ js/index.js
@@ -31,21 +31,9 @@ window.addEventListener('scroll', () => {
     function addVideoSource() {
       const videoPlayer = document.querySelector('.video_bg');
       const videoSrc = videoPlayer.dataset.src;
-      const videoNumber = 4;
-      var activeVideoNumber = 1;
-  
-      var webmSource = document.createElement('source');
-      webmSource.setAttribute('src', videoSrc + '.webm');
-      webmSource.setAttribute('type', 'video/webm');
-      videoPlayer.appendChild(webmSource);
-  
-      var ogvSource = document.createElement('source');
-      ogvSource.setAttribute('src', videoSrc + '.ogv');
-      ogvSource.setAttribute('type', 'video/ogg');
-      videoPlayer.appendChild(ogvSource);
-  
+    
       var mpSource = document.createElement('source');
-      mpSource.setAttribute('src', videoSrc + '.mp4');
+      mpSource.setAttribute('src', videoSrc);
       mpSource.setAttribute('type', 'video/mp4');
       videoPlayer.appendChild(mpSource);
   
@@ -56,8 +44,7 @@ window.addEventListener('scroll', () => {
       });
   
       function videoEnded() {
-        activeVideoNumber = ++activeVideoNumber % (videoNumber + 1) || 1;
-        videoPlayer.src = videoPlayer.currentSrc.replace(/\d(?=(.webm|.ogv|.mp4))/, activeVideoNumber);
+        videoPlayer.src = videoPlayer.currentSrc;
         videoPlayer.load();
         videoPlayer.play();
       }
